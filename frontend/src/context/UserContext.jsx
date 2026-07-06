43b766b3a96dcb1d8de8c4470b9fcb6ab5b04d89
Ravi Sharma <225026759+Ravi98777@users.noreply.github.com>
2026-07-07 02:23:44 +0530

    Fix serverUrl string format in UserContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const userDataContext = createContext();
 
 const UserContext = ({ children }) => {
-  const serverUrl = https://virtual-assistant-backend-myx0.onrender.com
+  const serverUrl =  "https://virtual-assistant-backend-myx0.onrender.com"
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
